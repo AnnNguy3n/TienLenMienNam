0a6d7762afaa6154012d82256de381ef6fa91128
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2022-05-22 15:34:18 +0700

    sửa state

--- gym_TLMN/envs/agents/MA_2.py
+++ gym_TLMN/envs/agents/MA_2.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 class Agent(Player):
     def __init__(self, name):
-        pd.DataFrame({'state':[], 'action':[], 'list_action': [],'win': []}).to_csv('State_tam_2.csv', index = False)
+        pd.DataFrame({'state':[], 'Turn_to_win': []}).to_csv('State_tam_2.csv', index = False)
         super().__init__(name)
 
     def action(self,  state=None):
@@ -19,24 +19,17 @@ class Agent(Player):
         try:
             s_a = pd.read_csv('State_tam_2.csv')
         except:
-            s_a = [np.nan, np.nan, np.nan, np.nan]
-        s_a.loc[len(s_a.index)] =[t, number, a, np.nan]
+            s_a = [np.nan]
+        s_a.loc[len(s_a.index)] =[t, np.nan]
         s_a.to_csv('State_tam_2.csv', index = False)
 
         if self.check_victory(t) == 1:
             try:
                 state_save = pd.read_csv('state.csv')
             except:
-                state_save = pd.DataFrame({'state':[], 'action':[], 'list_action': [],'win': []})
-            s_a = pd.read_csv(f'State_tam_2.csv').assign(win = 1)
-            state_save = pd.concat([state_save, s_a])
-            state_save.to_csv('state.csv', index = False)  
-        elif self.check_victory(t) == 0:
-            try:
-                state_save = pd.read_csv('state.csv')
-            except:
-                state_save = pd.DataFrame({'state':[], 'action':[], 'list_action': [],'win': []})
-            s_a = pd.read_csv(f'State_tam_2.csv').assign(win = 0)
-            state_save = pd.concat([state_save, s_a])  
-            state_save.to_csv('state.csv', index = False)       
+                state_save = pd.DataFrame({'state':[], 'Turn_to_win': []})
+            s_a = pd.read_csv(f'State_tam_2.csv')
+            s_a['Turn_to_win'] = s_a.index
+            state_save = pd.concat([state_save, s_a]) 
+            state_save.to_csv('state.csv', index = False)   
         return a[number]
