06d9d4a64150f0d8d98ef9dc4c6c7e3424150c86
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2022-05-22 15:50:46 +0700

    all

---
+++ main.py
@@ -0,0 +1,31 @@
+from copy import deepcopy
+import gym
+import gym_TLMN
+import time
+
+
+env = gym.make('gym_TLMN-v0')
+
+def main():
+    env.reset()
+
+    # print([i.name for i in env.players])
+
+    for i in range(300):
+        env.render()
+
+        o,a,done,t = env.step(env.turn.action(deepcopy(env.dict_input)))
+        if done:
+            break
+
+    for i in range(env.players.__len__()):
+        env.render()
+
+        o,a,done,t = env.step(env.turn.action(deepcopy(env.dict_input)))
+
+start = time.time()
+for i in range(1):
+    main()
+
+end = time.time()
+print(end - start, 'sec')
