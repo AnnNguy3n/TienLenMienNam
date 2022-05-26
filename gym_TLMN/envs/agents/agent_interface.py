c4d12ea71f65a88be658d20c8cba01e69461c88d
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2022-05-26 19:48:41 +0700

    sua json

--- gym_TLMN/envs/agents/agent_interface.py
+++ gym_TLMN/envs/agents/agent_interface.py
@@ -1,7 +1,12 @@
-from gym_TLMN.envs.agents import random as p1
-from gym_TLMN.envs.agents import agent_random as p2
-from gym_TLMN.envs.agents import random as p3
-from gym_TLMN.envs.agents import random as p4
+from gym_TLMN.envs.agents import S_a as p1
+from gym_TLMN.envs.agents import S_a as p2
+from gym_TLMN.envs.agents import S_a as p3
+from gym_TLMN.envs.agents import S_a as p4
+
+agent1 = p1.Agent('Phong')
+agent2 = p2.Agent('Hiếu')
+agent3 = p3.Agent('random1')
+agent4 = p4.Agent('random2')
 
 # from gym_TLMN.envs.agents import random as p1
 # from gym_TLMN.envs.agents import random as p2
